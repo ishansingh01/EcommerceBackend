aa1dca9ce3f4b36b4041ab64e31adcc8cb31c51e
ishansingh08 <ishansingh453@gmail.com>
2026-09-04 21:26:51 +0530

    error in securityconfig file

--- src/main/java/com/ishan/Ecomm/config/SecurityConfig.java
+++ src/main/java/com/ishan/Ecomm/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/user/register",
                                 "/user/login", "/error")
                         .permitAll()
